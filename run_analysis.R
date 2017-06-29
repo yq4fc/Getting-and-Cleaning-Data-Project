4db32eccfd4ee70f51b1c93f5f4b8979915dbe1d
yq4fc <yq4fc@mst.edu>
2017-06-29 02:52:47 -0500

    Run Analysis

--- run_analysis.R
+++ run_analysis.R
@@ -12,7 +12,6 @@ colnames(subjectTrain)  = "subjectId"
 colnames(xTrain)        = features[,2] 
 colnames(yTrain)        = "activityId"
 
-# Create the final training set by merging yTrain, subjectTrain, and xTrain
 trainingData = cbind(yTrain,subjectTrain,xTrain)
 
 # Read in the test data
@@ -20,13 +19,9 @@ subjectTest = read.table('./test/subject_test.txt',header=FALSE)
 xTest       = read.table('./test/x_test.txt',header=FALSE) 
 yTest       = read.table('./test/y_test.txt',header=FALSE) 
 
-# Assign column names to the test data imported above
 colnames(subjectTest) = "subjectId"
 colnames(xTest)       = features[,2]
 colnames(yTest)       = "activityId"
-
-
-# Create the final test set by merging the xTest, yTest and subjectTest data
 testData = cbind(yTest,subjectTest,xTest)
 
 
@@ -50,30 +45,14 @@ finalData = finalData[logicalVector==TRUE]
 # Merge the finalData set with the acitivityType table to include descriptive activity names
 finalData = merge(finalData,activityType,by='activityId',all.x=TRUE)
 
-# Updating the colNames vector to include the new column names after merge
-colNames  = colnames(finalData)
 
 # 4. Appropriately label the data set with descriptive activity names. 
-
-# Cleaning up the variable names
-for (i in 1:length(colNames)) 
-{
-  colNames[i] = gsub("\\()","",colNames[i])
-  colNames[i] = gsub("-std$","StdDev",colNames[i])
-  colNames[i] = gsub("-mean","Mean",colNames[i])
-  colNames[i] = gsub("^(t)","time",colNames[i])
-  colNames[i] = gsub("^(f)","freq",colNames[i])
-  colNames[i] = gsub("([Gg]ravity)","Gravity",colNames[i])
-  colNames[i] = gsub("([Bb]ody[Bb]ody|[Bb]ody)","Body",colNames[i])
-  colNames[i] = gsub("[Gg]yro","Gyro",colNames[i])
-  colNames[i] = gsub("AccMag","AccMagnitude",colNames[i])
-  colNames[i] = gsub("([Bb]odyaccjerkmag)","BodyAccJerkMagnitude",colNames[i])
-  colNames[i] = gsub("JerkMag","JerkMagnitude",colNames[i])
-  colNames[i] = gsub("GyroMag","GyroMagnitude",colNames[i])
-};
-
-# Reassigning the new descriptive column names to the finalData set
-colnames(finalData) = colNames
+names(finalData) = gsub("\\()","",names(finalData))
+names(finalData)= gsub("^(t)","time",names(finalData))
+names(finalData)= gsub("^(f)","freq",names(finalData))
+names(finalData)=gsub("([Bb]ody[Bb]ody|[Bb]ody)","Body",names(finalData))
+names(finalData) = gsub("Mag","Magnitude",names(finalData))
+names(finalData) <-gsub("Acc", "Accelerometer", names(finalData))
 
 # 5. Create a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
