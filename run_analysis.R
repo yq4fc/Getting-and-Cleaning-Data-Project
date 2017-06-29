154c935468e33c7e84ff2e2846e1fb966eb6fe13
yq4fc <yq4fc@mst.edu>
2017-06-29 03:10:12 -0500

    R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 # 1. Merge the training and the test sets to create one data set.
-# Read in the data from files
+# Load the data from files
 features     = read.table('./features.txt',header=FALSE)
 activityType = read.table('./activity_labels.txt',header=FALSE) 
 subjectTrain = read.table('./train/subject_train.txt',header=FALSE)
 xTrain       = read.table('./train/x_train.txt',header=FALSE)
 yTrain       = read.table('./train/y_train.txt',header=FALSE)
 
-# Assigin column names to the data imported above
+# Assigin column names to the data
 colnames(activityType)  = c('activityId','activityType')
 colnames(subjectTrain)  = "subjectId"
 colnames(xTrain)        = features[,2] 
@@ -14,7 +14,7 @@ colnames(yTrain)        = "activityId"
 
 trainingData = cbind(yTrain,subjectTrain,xTrain)
 
-# Read in the test data
+# Load the test data
 subjectTest = read.table('./test/subject_test.txt',header=FALSE) 
 xTest       = read.table('./test/x_test.txt',header=FALSE) 
 yTest       = read.table('./test/y_test.txt',header=FALSE) 
@@ -23,7 +23,6 @@ colnames(subjectTest) = "subjectId"
 colnames(xTest)       = features[,2]
 colnames(yTest)       = "activityId"
 testData = cbind(yTest,subjectTest,xTest)
-
 
 # Combine training and test data to create a final data set
 AllData = rbind(trainingData,testData)
@@ -36,11 +35,9 @@ meanAndstd <- (grepl("activityId" , colNames) |
                  grepl("std.." , colNames) 
 )
 finalData <- AllData[ , meanAndstd== TRUE]
+
 # 3. Use descriptive activity names to name the activities in the data set
-
-# Merge the finalData set with the acitivityType table to include descriptive activity names
 finalData = merge(finalData,activityType,by='activityId',all.x=TRUE)
-
 
 # 4. Appropriately label the data set with descriptive activity names. 
 names(finalData) = gsub("\\()","",names(finalData))
@@ -52,15 +49,15 @@ names(finalData) <-gsub("Acc", "Accelerometer", names(finalData))
 
 # 5. Create a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
-# Create a new table, finalDataNoActivityType without the activityType column
+# Create a new table without the activityType column
 finalDataNoActivityType  = finalData[,names(finalData) != 'activityType']
 
-# Summarizing the finalDataNoActivityType table to include just the mean of each variable for each activity and each subject
+# Include just the mean of each variable for each activity and each subject
 tidyData    = aggregate(finalDataNoActivityType[,names(finalDataNoActivityType) != c('activityId','subjectId')],by=list(activityId=finalDataNoActivityType$activityId,subjectId = finalDataNoActivityType$subjectId),mean)
 
-# Merging the tidyData with activityType to include descriptive acitvity names
+# Include descriptive acitvity names
 tidyData    = merge(tidyData,activityType,by='activityId',all.x=TRUE)
 
-# Export the tidyData set 
+# Create the tidyData set 
 write.table(tidyData, './tidyData.txt',row.names=TRUE,sep='\t')
 
